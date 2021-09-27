181f51a86b75fb1d660d81e83ac585ec2d2a26cc
hillo123 <43943328+hillo123@users.noreply.github.com>
2021-09-27 13:58:52 -0400

    separating childroutes from single route module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//modules
+import { PagesRoutingModule } from './pages/pages.routing';
+
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
+
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
-import { ProgressComponent } from './pages/progress/progress.component';
+
+
 
 const router: Routes = [
-  {
-    path: '', component: PagesComponent,
-    children:[
-    { path: 'dashboard', component: DashboardComponent },
-    { path: 'progress', component: ProgressComponent },
-    { path: 'grafica1', component: Grafica1Component },
-    { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
-  ]
-  },
+
 
 
   { path: 'login', component: LoginComponent },
@@ -26,7 +20,10 @@ const router: Routes = [
 ]
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forRoot(router)],
+  imports: [
+    RouterModule.forRoot(router),
+    PagesRoutingModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
