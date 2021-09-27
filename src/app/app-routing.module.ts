b732d85469042aa238290be314c938ebd2c082a7
hillo123 <43943328+hillo123@users.noreply.github.com>
2021-09-27 14:03:33 -0400

    separating auth routing to a single file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,27 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 //modules
 import { PagesRoutingModule } from './pages/pages.routing';
-
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
+import { AuthRoutingModule } from './auth/auth.routing';
 
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 
 
+
 const router: Routes = [
 
-
-
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
   { path: '**', component: NopagefoundComponent }
+
 ]
 @NgModule({
   declarations: [],
   imports: [
     RouterModule.forRoot(router),
-    PagesRoutingModule
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
   exports: [RouterModule]
 })
